6e2329df1763bb59aae1171464b8882d09043e9a
Larry Adah <larryadah@gmail.com>
2025-12-04 14:55:05 +0100

    OIDC Test

--- infrastructure/workflow_summary.sh
+++ infrastructure/workflow_summary.sh
@@ -6,11 +6,6 @@
 echo "## Deployment Successful!" >> $GITHUB_STEP_SUMMARY
 echo "Here are your infrastructure details:" >> $GITHUB_STEP_SUMMARY
 echo "" >> $GITHUB_STEP_SUMMARY
-BOT_ID=$(terraform output -raw bot_id)
-BOT_ALIAS_ID=$(terraform output -raw bot_alias_id)
 echo '```terraform' >> $GITHUB_STEP_SUMMARY
-echo "bot_id = $BOT_ID" >> $GITHUB_STEP_SUMMARY
-echo "bot_alias_id = $BOT_ALIAS_ID" >> $GITHUB_STEP_SUMMARY
+terraform output >> $GITHUB_STEP_SUMMARY
 echo '```' >> $GITHUB_STEP_SUMMARY
-URL=$(terraform output -raw https_url)
-echo "**[Click to Open Chatbot]($URL)**" >> $GITHUB_STEP_SUMMARY
