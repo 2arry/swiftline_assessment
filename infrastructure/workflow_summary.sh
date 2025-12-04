48109a25b9830524d365d280888f8ccae44a5f14
Larry Adah <larryadah@gmail.com>
2025-12-04 14:53:05 +0100

    OIDC Test

--- infrastructure/workflow_summary.sh
+++ infrastructure/workflow_summary.sh
@@ -14,4 +14,3 @@ echo "bot_alias_id = $BOT_ALIAS_ID" >> $GITHUB_STEP_SUMMARY
 echo '```' >> $GITHUB_STEP_SUMMARY
 URL=$(terraform output -raw https_url)
 echo "**[Click to Open Chatbot]($URL)**" >> $GITHUB_STEP_SUMMARY
-
